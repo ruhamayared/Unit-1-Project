a8ff726c5a61aa5a8e4f4561afe2cec307aa0728
Ruhama Yared <ruhamayared1@gmail.com>
2022-10-22 12:24:56 -0400

    final api

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 
-const apiKey = "16A4EHTL0Krje4QHZ"
+const apiKey = "c6a2739983msha6bd6665781d45dp1a5b00jsnae6fd9ea4b95"
 
 
-function cocktailSearch(){
-    const url = ""
+const baseURL = "https://the-cocktail-db.p.rapidapi.com/random.php?"
+
+function drinkSearch(){
     
     //Make our request
-    $.ajax(url)
-    .then((cocktail) => {
-        console.log(cocktail)
+    $.ajax({
+        url: `${baseURL}`,
+        headers: {"X-RapidAPI-Key": `${apiKey}`}
+    })
+
+    .then((drinkName) => {
+        console.log(drinkName)
         }
     )
 }
 
-cocktailSearch()
-
-
+drinkSearch()
